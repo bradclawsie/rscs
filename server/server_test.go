8a924d5aed594b9d58a62f4d303e1e82756a3756
brad clawsie <brad@b7j0c.org>
2017-02-16 10:38:06 -0800

    godoc example

--- server/server_test.go
+++ server/server_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"github.com/bradclawsie/rscs/db"
 	"io"
 	"io/ioutil"
@@ -269,4 +270,40 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io
 	defer resp.Body.Close()
 
 	return resp, string(respBody)
+}
+
+func Example() {
+
+	// You need a sqlite file. We assume here that you have one with
+	// a pre-existing `kv` table, if not, look at rscs.go in this directory
+	// for an example of how to create an empty one.
+	var sqliteDBFile string
+	flag.StringVar(&sqliteDBFile, "db", "", "full path to sqlite db file")
+	flag.Parse()
+
+	if sqliteDBFile == "" {
+		log.Fatal(`use: example_daemon --db={sqlite db file}`)
+	}
+
+	// Create a DB instance for sqlite.
+	rscsDB, rscsDBErr := db.NewRscsDB(sqliteDBFile)
+	if rscsDBErr != nil {
+		log.Fatal(rscsDBErr)
+	}
+
+	// Create an http abstraction layer.
+	rscsServer, rscsSrvErr := NewRscsServer(rscsDB)
+	if rscsSrvErr != nil {
+		log.Fatal(rscsSrvErr)
+	}
+
+	// Instantiate a router.
+	rtr, rtrErr := rscsServer.NewRouter()
+	if rtrErr != nil {
+		log.Fatal(rtrErr.Error())
+	}
+
+	// Listen.
+	srv := &http.Server{Addr: ":8081", Handler: rtr}
+	log.Fatal(srv.ListenAndServe())
 }
